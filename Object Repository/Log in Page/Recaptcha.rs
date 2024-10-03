05336a96b0890ef5eb075b5aa634b24b0cfbfdb5
M Irsyad <44132642+monosik@users.noreply.github.com>
2024-10-03 08:21:15 +0700

    Create Log in test case

---
+++ Object Repository/Log in Page/Recaptcha.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Recaptcha</name>
+   <tag></tag>
+   <elementGuidId>9b3ce49a-4b64-4cfd-a6be-644f4d7ad4c9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;recaptcha-checkbox-border&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Log in Page/Register Button.rs
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Register Button</name>
+   <tag></tag>
+   <elementGuidId>fe21448f-3953-48b7-b1f4-781d0154d7dc</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//button/following::p[text()=&quot;Daftar&quot;]</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <smartLocatorEnabled>false</smartLocatorEnabled>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
